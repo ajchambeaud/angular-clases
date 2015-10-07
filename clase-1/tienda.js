a81e35633fce8ebe1cd8d37ad38a0eb0e12f872e
Ainu Chambeaud <ajchambeaud@gmail.com>
2015-10-07 14:19:48 -0300

    Archivos de la clase 1

---
+++ clase-1/tienda.js
@@ -0,0 +1,50 @@
+angular.module("tienda", [])
+
+  .controller("MainController", function(){
+    
+    this.listaTienda = [
+      { 
+          name : "Cloak of Agility",
+          cost : 10
+      },
+      { 
+          name : "Phage",
+          cost : 7
+      },
+      { 
+          name : "Pickaxe",
+          cost : 8
+      }    
+    ];
+  
+    this.listaEquipo = [
+      { 
+          name : "Archangel's Staff",
+          cost : 6
+      },
+      { 
+          name : "Tear of the Goddess",
+          cost : 12
+      },
+      { 
+          name : "Rod of Ages",
+          cost : 15
+      }
+    ];
+  
+    this.toggleSelect = function(item){
+      item.selected = !item.selected;  
+    };
+  
+    this.toggleLista = function(lista){
+      lista.selected = !lista.selected; 
+      lista.forEach(function(item){
+        item.selected = lista.selected;
+      });
+    };  
+  
+    this.move = function(origen, destino){
+    
+    };
+
+  });
