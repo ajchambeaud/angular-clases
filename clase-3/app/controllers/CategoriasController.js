e85fea3db731dab2a59ba20b1302e442f600e1c5
Ainu Chambeaud <ajchambeaud@gmail.com>
2015-10-26 08:47:44 -0300

    clase 3 files

--- clase-3/app/controllers/CategoriasController.js
+++ clase-3/app/controllers/CategoriasController.js
@@ -1,5 +1,23 @@
 var backendEcommerce = angular.module("backendEcommerce");
 
-backendEcommerce.controller("CategoriasController", function(){
+backendEcommerce.controller("CategoriasController", function(CategoriaService){
 
+	var self = this;
+	this.categorias = [];
+	this.selected = false;
+	this.categoria = {};
+	this.formLabel = "Nueva Categoria";
+	
+	this.select = function(categoria){
+		this.selected = categoria;
+	};
+	
+	this.isSelected = function(categoria){
+		return this.selected && this.selected.categoria_id == categoria.categoria_id;
+	};
+	
+	CategoriaService.get().then(function(data){
+		self.categorias = data;
+	});
+	
 });
--- clase-3/app/controllers/MainController.js
+++ clase-3/app/controllers/MainController.js
@@ -2,4 +2,6 @@ var backendEcommerce = angular.module("backendEcommerce");
 
 backendEcommerce.controller("MainController", function(){
     
+	this.brand = "Angular Services";
+	
 });
